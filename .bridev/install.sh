0b69e5e742a72e15cd56cab49596a9c520c977f0
agungdharmawangsabri <anakagung.dharmawangsa.konsultan@work.bri.co.id>
2024-05-03 11:58:59 +0700

    update command

--- .bridev/install.sh
+++ .bridev/install.sh
@@ -9,28 +9,29 @@ sudo apt dist-upgrade -y
 sudo apt install -y ca-certificates apt-transport-https software-properties-common lsb-release
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
-sudo apt upgrade -y \
-sudo apt install git php8.3 \
-    php8.3-dev \
-    php8.3-cli \
-    php8.3-fpm \
-    php8.3-common \
-    php8.3-zip \
-    php8.3-gd \
-    php8.3-intl \
-    php8.3-mbstring \
-    php8.3-curl \
-    php8.3-xml \
-    php-pear \
-    php8.3-tidy \
-    php8.3-soap \
-    php8.3-bcmath \
-    php8.3-pgsql \
-    php8.3-opcache \
-    php-imagick \
-    imagemagick \
-    webp \ 
-    php8.3-xmlrpc -y
+sudo apt upgrade -y
+sudo apt install -y git 
+sudo apt install -y php8.3
+sudo apt install -y php8.3-dev 
+sudo apt install -y php8.3-cli 
+sudo apt install -y php8.3-fpm 
+sudo apt install -y php8.3-common 
+sudo apt install -y php8.3-zip 
+sudo apt install -y php8.3-gd 
+sudo apt install -y php8.3-intl 
+sudo apt install -y php8.3-mbstring 
+sudo apt install -y php8.3-curl 
+sudo apt install -y php8.3-xml 
+sudo apt install -y php-pear 
+sudo apt install -y php8.3-tidy 
+sudo apt install -y php8.3-soap 
+sudo apt install -y php8.3-bcmath 
+sudo apt install -y php8.3-pgsql 
+sudo apt install -y php8.3-opcache 
+sudo apt install -y php-imagick 
+sudo apt install -y imagemagick 
+sudo apt install -y webp
+sudo apt install -y php-xmlrpc
 sudo service php8.3-fpm start
 # run other sh
-sh ../.devcontainer/devcommand.sh
+sh ./.bridev/command.sh
