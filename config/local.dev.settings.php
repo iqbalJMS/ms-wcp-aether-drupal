1360c1c4fa8cc1929095adce12567049685a9431
indescdev <devteam@indesc.com>
2024-07-15 21:24:25 +0800

    WCC-00: File settings.php

--- config/local.dev.settings.php
+++ config/local.dev.settings.php
@@ -892,7 +892,9 @@ $databases['default']['default'] = array(
   'namespace' => 'Drupal\\pgsql\\Driver\\Database\\pgsql',
   'autoload' => 'core/modules/pgsql/src/Driver/Database/pgsql/',
 );
-$settings['config_sync_directory'] = 'sites/default/files/config_Z5G3HlqxuiliMIa8zwFrYZmTcgbQmjFXY6ozq2Se1IbMir4TgLIwzLHzLfyU5mrkK8WqgPPOLg/sync';
+$settings['config_sync_directory'] = '../config/sync';
+$config_directories['sync'] = '../config/sync';
+
 // $settings['mongodb'] = [
 //   'clients' => [
 //     // Client alias => connection constructor parameters.
