94b2ba60343a9d8e4c535502cf3205fbd7f8d2d2
indescdev <devteam@indesc.com>
2024-08-06 19:36:22 +0800

    Card group nav

--- web/load.environment.php
+++ web/load.environment.php
@@ -2,6 +2,9 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
+if (file_exists(DRUPAL_ROOT . '/../.env')) {
+  (new Dotenv())->usePutenv()->bootEnv(DRUPAL_ROOT . '/../.env', 'dev', ['test'], true);
+}
 if (file_exists(DRUPAL_ROOT . '/../../.env')) {
   (new Dotenv())->usePutenv()->bootEnv(DRUPAL_ROOT . '/../../.env', 'dev', ['test'], true);
 }
--- web/web/modules/custom/bricc/src/Normalizer/CardSliderNormalizer.php
+++ web/web/modules/custom/bricc/src/Normalizer/CardSliderNormalizer.php
@@ -19,7 +19,7 @@ class CardSliderNormalizer extends ContentEntityNormalizer {
    *
    * @var array
    */
-  protected $supportedParagraphTypes = ['card_slider'];
+  protected $supportedParagraphTypes = ['card_slider', 'card_group_nav'];
 
   /**
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
@@ -64,15 +64,29 @@ class CardSliderNormalizer extends ContentEntityNormalizer {
       $context
     );
 
-    if ($entity->hasField('field_category')) {
-      if (!$entity->get('field_category')->isEmpty()) {
-        $field_category = $entity->get('field_category')->target_id;
-        // Get card item data based on category field.
-        $card_items = $this->em->getStorage('bricc_card_item')->loadByProperties([
-          'field_category' => $field_category,
-        ]);
-        $normalized['card_items'] = $this->serializer->normalize($card_items, 'json_recursive');
+    if ($entity->bundle() == 'card_slider') {
+      if ($entity->hasField('field_category')) {
+        if (!$entity->get('field_category')->isEmpty()) {
+          $field_category = $entity->get('field_category')->target_id;
+          // Get card item data based on category field.
+          $card_items = $this->em->getStorage('bricc_card_item')->loadByProperties([
+            'field_category' => $field_category,
+          ]);
+          $normalized['card_items'] = $this->serializer->normalize($card_items, 'json_recursive');
+        }
       }
+    }
+    elseif ($entity->bundle() == 'card_group_nav') {
+      // Get list of categories
+      $categories = $this->em->getStorage('bricc_category')->loadByProperties([
+        'status' => TRUE,
+      ]);
+      $category_items = [];
+      /** @var \Drupal\bricc\Entity\BriccCategory $category */
+      foreach ($categories as $category) {
+        $category_items[$category->id()] = $category->label();
+      }
+      $normalized['items'] = $category_items;
     }
 
     return $normalized;
