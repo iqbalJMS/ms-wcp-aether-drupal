54f4022741d9ea6c97be9e1a06c64d5ace32fb51
indescdev <devteam@indesc.com>
2024-08-08 19:53:18 +0800

    env loading

--- web/load.environment.php
+++ web/load.environment.php
@@ -2,9 +2,11 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-if (file_exists(DRUPAL_ROOT . '/../.env')) {
-  (new Dotenv())->usePutenv()->bootEnv(DRUPAL_ROOT . '/../.env', 'dev', ['test'], true);
-}
-elseif (file_exists(DRUPAL_ROOT . '/../../.env')) {
+if (file_exists(DRUPAL_ROOT . '/../../.env')) {
+  // Find in project root
   (new Dotenv())->usePutenv()->bootEnv(DRUPAL_ROOT . '/../../.env', 'dev', ['test'], true);
+}
+else {
+  // Find in composer
+  (new Dotenv())->usePutenv()->bootEnv(DRUPAL_ROOT . '/../.env', 'dev', ['test'], true);
 }
