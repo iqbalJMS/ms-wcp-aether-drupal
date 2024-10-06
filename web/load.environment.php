b11e77515799c6c01ccc1f87d36e7e34636bba2c
Permana Jayanta <permanaonline@gmail.com>
2024-10-06 22:59:28 +0800

    Add file check

--- web/load.environment.php
+++ web/load.environment.php
@@ -6,7 +6,7 @@ if (file_exists(DRUPAL_ROOT . '/../../.env')) {
   // Find in project root
   (new Dotenv())->usePutenv()->bootEnv(DRUPAL_ROOT . '/../../.env', 'dev', ['test'], true);
 }
-else {
+elseif (file_exists(DRUPAL_ROOT . '/../.env')) {
   // Find in composer
   (new Dotenv())->usePutenv()->bootEnv(DRUPAL_ROOT . '/../.env', 'dev', ['test'], true);
 }
